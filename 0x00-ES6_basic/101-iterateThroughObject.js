faefc338a384aad3a413039a7fed06b4c0a72ed2
Ezeuju <ezeuju1919@gmail.com>
2023-06-28 15:17:33 +0100

    linted

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,13 +1,12 @@
 export default function iterateThroughObject(reportWithIterator) {
-    let result = '';
-  
-    for (const item of reportWithIterator) {
-      result += `${item} | `;
-    }
-  
-    // Remove the trailing ' | ' characters
-    result = result.slice(0, -3);
-  
-    return result;
+  let result = '';
+
+  for (const item of reportWithIterator) {
+    result += `${item} | `;
   }
-  
+
+  // Remove the trailing ' | ' characters
+  result = result.slice(0, -3);
+
+  return result;
+}
