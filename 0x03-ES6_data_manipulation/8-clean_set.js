608366b2230174b292cb687e077037e4cdb7b138
Ezeuju <ezeuju1919@gmail.com>
2023-07-05 00:59:22 +0100

    clear set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,25 @@
-function cleanSet(set, startString) {
-  const filteredValues = [...set].filter((value) => value.startsWith(startString));
-  return filteredValues.join('-');
-}
+// function cleanSet(set, startString) {
+//   const filteredValues = [...set].filter((value) => value.startsWith(startString));
+//   return filteredValues.join('-');
+// }
 
-export default cleanSet;
+// export default cleanSet;
+export default function cleanSet(set, startString) {
+  const list = [];
+
+  if (
+    typeof set !== 'object'
+      || typeof startString !== 'string'
+      || startString.length === 0
+  ) {
+    return '';
+  }
+
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      list.push(item.slice(startString.length));
+    }
+  }
+
+  return list.join('-');
+}
