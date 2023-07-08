4b435a9e6eeb77498b9ef4b5190e8cfbc3ed7eae
Ezeuju <ezeuju1919@gmail.com>
2023-07-08 14:17:46 +0100

    my-first-commit

---
+++ 0x04-TypeScript/task_1/main.ts
@@ -0,0 +1,73 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  readonly fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+
+function printTeacher(firstName: string, lastName: string): string {
+  const firstLetter = firstName.charAt(0).toUpperCase();
+  const formattedLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+
+  return `${firstLetter}. ${formattedLastName}`;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+console.log(printTeacher("John", "Doe"));
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClass;
+}
+
+interface StudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClass {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass('John', 'Doe');
+console.log(student.workOnHomework());
+console.log(student.displayName());
+
+
+
---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,20 @@
+interface MajorCredits {
+    credits: number;
+    __brand: 'MajorCredits';
+  }
+  
+  interface MinorCredits {
+    credits: number;
+    __brand: 'MinorCredits';
+  }
+  
+  function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+    const totalCredits = subject1.credits + subject2.credits;
+    return { credits: totalCredits, __brand: 'MajorCredits' };
+  }
+  
+  function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+    const totalCredits = subject1.credits + subject2.credits;
+    return { credits: totalCredits, __brand: 'MinorCredits' };
+  }
+  
